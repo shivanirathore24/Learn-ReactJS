d135d90e8546ef4aa8da0e5c9a241fe617cdb3ff
shivanirathore24 <shivani.rathore2409@gmail.com>
2024-08-29 21:29:32 +0530

    React Hooks: Project Setup

---
+++ React-Hooks/src/App.js
@@ -0,0 +1,13 @@
+import Input from "./Components/InputWithClass";
+
+function App() {
+  return (
+    <>
+      <Input />
+
+     
+    </>
+  );
+}
+
+export default App;
---
+++ React-Hooks/src/Components/InputWithFunction.js
@@ -0,0 +1,30 @@
+export default function Input() {
+  return (
+    <>
+      <div className="section">
+        <Row label="Name">
+          <input className="input" />
+        </Row>
+        <Row label="Last Name">
+          <input className="input" />
+        </Row>
+      </div>
+
+      <h2>Hello, </h2>
+    </>
+  );
+}
+
+function Row(props) {
+  const { label } = props;
+  return (
+    <>
+      <lable>
+        {label}
+        <br />
+      </lable>
+      {props.children}
+      <hr />
+    </>
+  );
+}
