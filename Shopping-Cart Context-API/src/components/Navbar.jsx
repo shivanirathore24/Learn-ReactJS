fce0d9dd925e8b57c47de67e3b7c2283be458dae
shivanirathore24 <shivani.rathore2409@gmail.com>
2024-10-11 23:15:06 +0530

    Shopping-Cart Context-API: Removing Multiple Context

--- Shopping-Cart Context-API/src/components/Navbar.jsx
+++ Shopping-Cart Context-API/src/components/Navbar.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import styles from "../styles/Total.module.css";
 import { useContext } from "react";
 import { itemContext } from "../itemContext";
-import { totalContext } from "../totalContext";
 
 function Navbar() {
-  const {item} = useContext(itemContext);
-  const {total} = useContext(totalContext);
+  const {total, item} = useContext(itemContext);
   return (
     <div className={styles.container}>
       <h1>Total : &#x20B9; {total}</h1>
--- Shopping-Cart Context-API/src/totalContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const totalContext = createContext();
-export { totalContext };
